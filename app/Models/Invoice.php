36331aaeb22ed2c96ace9e9f1249b738b9bf2155
Jose Alejandro Melendez Garcia <jmelendez490@gmail.com>
2024-12-26 21:39:52 -0600

    Date lock enabled

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Traits\GeneratesPDFFile;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
@@ -29,12 +30,16 @@ class Invoice extends Model
     {
         parent::boot();
 
-        self::creating(function($model) {
+        self::creating(function(Invoice $model) {
             /** @var int $limit */
             $limit = (int) env('INVOICE_LIMIT', 10000);
 
             if (Invoice::latest()->first()->id >= $limit) {
                 throw new \Exception("Invoice limit of {$limit} reached");
+            }
+
+            if (now() > Carbon::parse(env('EMISSION_LIMIT_DATE', '10/12/2025'))) {
+                throw new \Exception("Emission limit date reached");
             }
         });
     }
