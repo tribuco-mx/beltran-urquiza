d279ac3bd6e2bd628a3b75cfe114d309b77eb128
Luis <ldaldana001@gmail.com>
2025-02-25 12:35:37 -0600

    Changed email unique in migration

--- database/migrations/2024_11_05_213058_create_customers_table.php
+++ database/migrations/2024_11_05_213058_create_customers_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('tax_id')->nullable();  // Tax ID
             $table->string('billing_address')->nullable();  // Billing Address
             $table->string('province')->nullable();  // Province
-            $table->string('email')->unique();  // Client Email
+            $table->string('email');  // Client Email
             $table->string('payment_method')->nullable();  // Payment Method
             $table->string('cardholder')->nullable();  // Cardholder
             $table->string('card_type')->nullable();  // Card Type
