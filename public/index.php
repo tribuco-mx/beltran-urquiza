6e346c01b34b7feff720c21b1ab228719868bef1
Luis <ldaldana001@gmail.com>
2025-03-07 13:30:51 -0600

    Aumented memory limit

--- public/index.php
+++ public/index.php
@@ -9,6 +9,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+ini_set('memory_limit', '500M');
+
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
