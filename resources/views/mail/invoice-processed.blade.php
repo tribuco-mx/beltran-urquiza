9beb2e4388bb70c349dd8965058d037b5bf8441f
Luis <ldaldana001@gmail.com>
2025-03-18 16:38:33 -0600

    Added translations

--- resources/views/mail/invoice-processed.blade.php
+++ resources/views/mail/invoice-processed.blade.php
@@ -60,11 +60,9 @@ td {
 padding: 5px;
 }
 </style>
-    <div>
-        <p style="margin-top: 10px; font-size: 10pt; font-weight: bold;">Anexo factura fiscal por el servicio de
-            mensajería
-            de entrega del pasaporte.</p>
-    </div>
+<div>
+<p style="margin-top: 10px; font-size: 10pt; font-weight: bold;">Anexo factura fiscal por el servicio de mensajería de entrega del pasaporte.</p>
+</div>
 <div class="invoice-container">
 {{--@if(env('APP_ENV') !== 'production')--}}
 {{--<div style="--}}
