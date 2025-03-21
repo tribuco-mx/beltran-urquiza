79881229e76db093ff45bea18f961304d0a09618
Luis <ldaldana001@gmail.com>
2025-03-20 20:27:54 -0600

    Increased chunk size

--- app/Filament/Resources/InvoiceResource/Pages/ListInvoices.php
+++ app/Filament/Resources/InvoiceResource/Pages/ListInvoices.php
@@ -19,6 +19,7 @@ class ListInvoices extends ListRecords
         return [
             Actions\ImportAction::make()
                 ->importer(importer: InvoiceImporter::class)
+                ->chunkSize(500)
                 ->headerOffset(4),
             Actions\CreateAction::make(),
             Actions\Action::make('export_invoices')
