e2a98dfe3216500e3a937c9cfc7b6980dd3ef2eb
Luis <ldaldana001@gmail.com>
2025-07-31 12:29:19 -0600

    Updated output csv

--- resources/views/mail/invoice-processed.blade.php
+++ resources/views/mail/invoice-processed.blade.php
@@ -100,7 +100,7 @@ padding: 5px;
 <td><p class="s1">CAI: {{ env('CAI', '28EF0B-3354D1-CC7BE0-63BE03-0909D6-EA') }}</p></td>
 </tr>
 <tr>
-<td><p class="s1">Rango autorizado: {{ env('AUTHORIZED_RANGE', '000-001-01-00000001 / 000-001-01-00010000') }}</p></td>
+<td><p class="s1">Rango autorizado: {{ env('AUTHORIZED_RANGE', '000-001-01-00010001 / 000-001-01-00040000') }}</p></td>
 </tr>
 <tr>
 <td><p class="s1">Fecha límite de emisión:  {{ env('EMISSION_LIMIT_DATE', '10/12/2025') }}</p></td>
