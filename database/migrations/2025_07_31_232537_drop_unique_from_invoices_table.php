c2d574004b116fe2036a20d57ebb2160f862ed89
Luis <ldaldana001@gmail.com>
2025-07-31 17:43:38 -0600

    Updated messages and importer to allow duplicate order_id invoices

---
+++ database/migrations/2025_07_31_232537_drop_unique_from_invoices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropUnique('invoices_order_id_unique');
+            $table->dropUnique('invoices_transaction_ref_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->string('order_id')->unique()->change();
+            $table->string('transaction_ref')->unique()->change();
+        });
+    }
+
+};
