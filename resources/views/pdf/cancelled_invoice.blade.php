1c922e3a92000180d4ca7b9645fd5cd4e1da957b
Luis <ldaldana001@gmail.com>
2025-08-01 17:35:58 -0600

    cHANGES TO CANCELLED INVOICE

--- resources/views/pdf/cancelled_invoice.blade.php
+++ resources/views/pdf/cancelled_invoice.blade.php
@@ -39,11 +39,11 @@
                 <p style="color: black; font-size: 8pt;">Número de teléfono: {{ $invoice->company->phone }}</p>
                 <p style="color: black; font-size: 8pt;">Correo electrónico: {{ $invoice->company->email }}</p>
                 <p style="color: black; font-size: 8pt;">
-                    CAI: {{ env('CAI', '28EF0B-3354D1-CC7BE0-63BE03-0909D6-EA') }}</p>
+                    CAI: 28EF0B-3354D1-CC7BE0-63BE03-0909D6-EA</p>
                 <p style="color: black; font-size: 8pt;">Rango
-                    autorizado: {{ env('AUTHORIZED_RANGE', '30000 000-001-01-00010001 / 000-001-01-00040000') }}</p>
+                    autorizado: 30000 000-001-01-00010001 / 000-001-01-00040000</p>
                 <p style="color: black; font-size: 8pt;">Fecha límite de
-                    emisión: {{ env('EMISSION_LIMIT_DATE', '15/07/2026') }} </p>
+                    emisión: 15/07/2026</p>
             </td>
             <td style="width: 50%; vertical-align: top;">
                 <p style="color: black; font-weight: bold; font-size: 8pt;">FECHA DE FACTURA:</p>
