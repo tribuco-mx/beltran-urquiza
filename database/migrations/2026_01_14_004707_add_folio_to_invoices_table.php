48ed7bea7692ce1ec7a3c43d47bc888b44cf75a5
Luis <ldaldana001@gmail.com>
2026-01-26 13:53:11 -0600

    Updated folio

--- database/migrations/2026_01_14_004707_add_folio_to_invoices_table.php
+++ database/migrations/2026_01_14_004707_add_folio_to_invoices_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('invoices', function (Blueprint $table) {
-            $table->string('folio')->after('status')->unique()->nullable();
+            $table->string('folio')->unique()->nullable();
         });
     }
 
